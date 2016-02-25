4179d38b2cd01376730452a2ca0c273a87affcf5
Luis Alberto Concha Curay <luisconchacuray@gmail.com>
2016-02-24 21:13:14 -0300

    testando teste unitarios

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,14 +6,34 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic functional test example.
-     *
-     * @return void
-     */
-    public function testBasicExample()
-    {
-        $this->visit('/')
-             ->see('Laravel 5');
-    }
+		/**
+		 * A basic functional test example.
+		 *
+		 * @return void
+		 */
+		public function testBasicExample()
+		{
+				$this->visit( '/' )
+						->see( 'Laravel 5' );
+		}
+
+		public function testOlaMundo()
+		{
+				$this->visit( '/ola' )
+						->see( 'Olá mundo!' );
+		}
+
+		public function testPostRoute()
+		{
+				$this->post( '/post', [
+						'name'  => 'Luis Alberto',
+						'idade' => '23',
+						'email' => 'luvett1@gmal.com'
+				] )->seeStatusCode( 200 )
+					 ->seeJson([
+							 'name'  => 'Luis Alberto',
+							 'idade' => '23',
+							 'email' => 'luvett1@gmal.com'
+					 ]);
+		}
 }
