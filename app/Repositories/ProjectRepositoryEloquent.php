988c2daa9f14b8f14bcaa40712ccde60947e1d65
Luis Alberto Concha Curay <luisconchacuray@gmail.com>
2016-03-05 22:11:01 -0300

    Verificando se usuario é membro do projeto a ser listado

--- app/Repositories/ProjectRepositoryEloquent.php
+++ app/Repositories/ProjectRepositoryEloquent.php
@@ -30,4 +30,17 @@ class ProjectRepositoryEloquent extends BaseRepository implements ProjectReposit
 
 				return false;
 		}
+
+		public function hasMember( $projectId, $memberId )
+		{
+				$project = $this->find( $projectId );
+
+				foreach ( $project->members as $member ) :
+						if ( $member->id == $memberId ):
+								return true;
+						endif;
+				endforeach;
+
+				return false;
+		}
 }
