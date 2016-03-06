8207e7af05468eec0d3e689d3a86db1fffca6859
Luis Alberto Concha Curay <luisconchacuray@gmail.com>
2016-03-06 16:09:44 -0300

    Criação do arquivo de mimes para validação de anexos

---
+++ config/mimes.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+		'doc'  => array( 'application/msword', 'application/vnd.ms-office' ),
+		'docx' => array( 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/zip' ),
+		'xlsx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+		'word' => array( 'application/msword', 'application/octet-stream' ),
+		'pdf'  => array( 'application/pdf', 'application/x-download' ),
+		'gif'  => 'image/gif',
+		'jpeg' => array( 'image/jpeg', 'image/pjpeg' ),
+		'jpg'  => array( 'image/jpeg', 'image/pjpeg' ),
+		'jpe'  => array( 'image/jpeg', 'image/pjpeg' ),
+		'png'  => 'image/png',
+];
