75f873be3fb221d38ab9aea6d75799b94fa6496d
Luis Alberto Concha Curay <luisconchacuray@gmail.com>
2016-03-20 15:58:03 -0300

    finalizando funcionalidade de edit do client

--- public/build/js/services/client.js
+++ public/build/js/services/client.js
@@ -2,7 +2,8 @@ angular.module( 'app.services' )
     .service( 'Client', [ '$resource', 'appConfig', function ( $resource, appConfig ) {
         return $resource( appConfig.baseUrl + '/clients/:id', { id: '@id' }, {
             update: {
-                method: 'PUT'
+                method: 'PUT', 
+                isArray: true
             }
         } );
     } ] );
