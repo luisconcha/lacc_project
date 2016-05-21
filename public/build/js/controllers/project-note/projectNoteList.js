085a5fd110598a49ad380658d9b62a821548bc0f
Luis Alberto Concha Curay <luisconchacuray@gmail.com>
2016-05-20 22:04:16 -0300

    Ajustes do bug ao cadastrar nota do projeto por primeira vez

--- public/build/js/controllers/project-note/projectNoteList.js
+++ public/build/js/controllers/project-note/projectNoteList.js
@@ -2,9 +2,12 @@ angular.module( 'app.controllers' )
     .controller( 'ProjectNoteListController', [
         '$scope', '$routeParams', 'ProjectNote',
         function ( $scope, $routeParams, ProjectNote ) {
-            console.log('Obj: ',$routeParams );
+            
+
             ProjectNote.getProjectNote( { id: $routeParams.id }, function ( data ) {
-                console.log('Obj: ',data );
+            
+                $scope.projectId = $routeParams.id;
+                
                 if( data.length > 0 ){
                     $scope.nameProject  = data[0].project_name;
                     $scope.projectNotes = data;
