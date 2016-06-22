414d58539cc425974b2e38a918dc94db638af9a4
Luis Alberto Concha Curay <luisconchacuray@gmail.com>
2016-06-22 00:28:23 -0300

    implementação do dashboard de projeto - pagina home

--- public/build/js/controllers/home.js
+++ public/build/js/controllers/home.js
@@ -1,4 +1,23 @@
 angular.module( 'app.controllers' )
-    .controller( 'HomeController', [ '$scope', '$cookies', function ( $scope, $cookies ) {
-            $scope.user_name = $cookies.getObject( 'user' ).user_name;
+    .controller( 'HomeController', [ '$scope', '$cookies', 'Project', function ( $scope, $cookies, Project ) {
+
+        $scope.user_name = $cookies.getObject( 'user' ).user_name;
+
+        Project.get( {
+            orderBy: 'created_at',
+            sortedBy: 'desc',
+            limit: 5
+        }, function ( data ) {
+            console.log( 'Obj:: ', data );
+            $scope.projects = data.data;
+        } );
+
+        $scope.alterViewProjects = function ( typeView ) {
+            if ( typeView == 'viewList' ) {
+                $('.div-list-project' ).removeClass('col-sm-4' ).addClass('col-sm-12');
+            } else if ( typeView == 'viewPanel' ) {
+                $('.div-list-project' ).removeClass('col-sm-12' ).addClass('col-sm-4');
+            }
+        };
+
     } ] );
