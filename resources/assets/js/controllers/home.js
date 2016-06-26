a40444536f6f865fed2341f6d8202e86c89f38e1
Luis Alberto Concha Curay <luisconchacuray@gmail.com>
2016-06-26 14:00:59 -0300

    paginação na listagem de projects no home

--- resources/assets/js/controllers/home.js
+++ resources/assets/js/controllers/home.js
@@ -1,22 +1,39 @@
 angular.module( 'app.controllers' )
     .controller( 'HomeController', [ '$scope', '$cookies', 'Project', function ( $scope, $cookies, Project ) {
 
-        $scope.user_name = $cookies.getObject( 'user' ).user_name;
+        $scope.user_name       = $cookies.getObject( 'user' ).user_name;
+        $scope.projects        = [];
+        $scope.totalProjects   = 0;
+        $scope.projectsPerPage = 6;
 
-        Project.get( {
-            orderBy: 'created_at',
-            sortedBy: 'desc',
-            limit: 5
-        }, function ( data ) {
-            console.log( 'Obj:: ', data );
-            $scope.projects = data.data;
-        } );
+        $scope.pagination = {
+            current: 1
+        };
+
+        //Quando usuario clicar em uma páginação
+        $scope.pageChanged = function ( newPage ) {
+            getResultsPage( newPage );
+        };
+
+        function getResultsPage( pageNumber ) {
+            Project.getProject( {
+                page: pageNumber,
+                limit: $scope.projectsPerPage
+            }, function ( data ) {
+                console.log( 'Obj:: ', data );
+                $scope.projects      = data.data;
+                $scope.totalProjects = data.meta.pagination.total;
+            } );
+        }
+
+        //Chama a função na primeira página
+        getResultsPage( 1 );
 
         $scope.alterViewProjects = function ( typeView ) {
             if ( typeView == 'viewList' ) {
-                $('.div-list-project' ).removeClass('col-sm-4' ).addClass('col-sm-12');
+                $( '.div-list-project' ).removeClass( 'col-sm-4' ).addClass( 'col-sm-12' );
             } else if ( typeView == 'viewPanel' ) {
-                $('.div-list-project' ).removeClass('col-sm-12' ).addClass('col-sm-4');
+                $( '.div-list-project' ).removeClass( 'col-sm-12' ).addClass( 'col-sm-4' );
             }
         };
 
