c998ad7718c38f6e197646a0b64f13048f7b6194
luisconcha <luisconchacuray@gmail.com>
2016-11-27 22:38:16 -0200

    Alterando a forma como pegar os dados da service, colocando nome do projeto na lateral do grafico

--- resources/assets/js/controllers/reports/reports.js
+++ resources/assets/js/controllers/reports/reports.js
@@ -1,17 +1,38 @@
 angular.module( 'app.controllers' )
     .controller( 'ReportsController', [ '$scope', 'Reports', function ( $scope, Reports ) {
 
+        // $scope.projectResultPie = [];
+        // $scope.pieData =  Reports.getProjects( {}, function () {} );
+        // $scope.pieData.$promise.then(function ( result ) {
+        //     angular.forEach( result, function ( value, key ) {
+        //         $scope.projectResultPie.push( {
+        //             name: value.name,
+        //             y: value.progress
+        //         } );
+        //     } );
+        // });
+
         $scope.projectResultPie = [];
-        $scope.pieData =  Reports.getProjects( {}, function () {} );
-        $scope.pieData.$promise.then(function ( result ) {
-            angular.forEach( result, function ( value, key ) {
+        $scope.categories       = [];
+        Reports.getProjects( {
+            id: {}
+        }, function ( data ) {
+            /**
+             * Percorre os valores para montar o grafico
+             */
+            angular.forEach( data, function ( value, key ) {
                 $scope.projectResultPie.push( {
                     name: value.name,
                     y: value.progress
                 } );
             } );
-        });
-
+            /**
+             * Percorre os valores para montar a coluna com os titulos do projeto
+             */
+            angular.forEach( data, function ( value ) {
+                $scope.categories.push( value.name );
+            } );
+        } );
 
         $scope.chartProject = {
             options: {
@@ -19,14 +40,14 @@ angular.module( 'app.controllers' )
                     type: 'bar'
                 }
             },
-            series: [{
+            series: [ {
                 data: $scope.projectResultPie
-            }],
+            } ],
             title: {
                 text: 'Project Report'
             },
             xAxis: {
-                categories: ['1', '2', '3', '4', '5'],
+                categories: $scope.categories,
                 title: {
                     text: 'Nome do Projeto'
                 }
@@ -47,7 +68,6 @@ angular.module( 'app.controllers' )
             credits: {
                 enabled: false
             },
-
             loading: false
         };
 
